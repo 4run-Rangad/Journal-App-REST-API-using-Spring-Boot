5c8266af840589953fa478ed7e4c48baa5fc56d0
4run-Rangad <arunrangad83@gmail.com>
2026-07-02 21:51:43 +0530

    Implemented unit testing using Junit

---
+++ src/test/java/com/arunrangad/journalApp/service/UserServiceTests.java
@@ -0,0 +1,27 @@
+package com.arunrangad.journalApp.service;
+import com.arunrangad.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTests {
+
+@Autowired
+private UserRepository userRepository;
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Ram",
+            "Shyam",
+            "Arun"
+    })
+    public void testFindByUserName(String name){
+        assertNotNull(userRepository.findByUserName(name), "failed for name: "+ name);
+    }
+}
