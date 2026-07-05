88d0a2b59e4166870e8361cd4e977580e2a1f907
4run-Rangad <arunrangad83@gmail.com>
2026-07-05 15:05:44 +0530

    Implemented unit testing using JUNIt and Mockito

---
+++ src/test/java/com/arunrangad/journalApp/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.arunrangad.journalApp.service;
+
+import com.arunrangad.journalApp.entity.User;
+import com.arunrangad.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import java.util.ArrayList;
+import static org.mockito.Mockito.*;
+
+
+public class UserDetailsServiceImplTest {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void loadUserByUserNameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("ram").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
