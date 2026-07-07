01428449bbab00c7b3058683694ff3430e10e521
4run-Rangad <arunrangad83@gmail.com>
2026-07-07 21:41:27 +0530

    Introduced Dev and Prod profiles

--- src/main/java/com/arunrangad/journalApp/config/securityProd.java
+++ src/main/java/com/arunrangad/journalApp/config/securityProd.java
@@ -6,11 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,8 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile(("dev"))
-public class security{
+@Profile(("prod"))
+public class securityProd {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
@@ -30,7 +28,7 @@ public class security{
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**","/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
